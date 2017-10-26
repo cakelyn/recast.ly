21a2efc4e5d8fe7d7b5428b170056dc51c71f21f
Katelyn Stamas <cakelyn@gmail.com>
2017-10-26 15:06:41 -0400

    computer crashed, safe save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,28 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      videos: true,
-      video: true
+      videos: this.props.videos,
+      video: this.props.videos[0]
     };
+  }
+
+  // function
+  test (currentVideo) {
+    this.setState({
+      video: currentVideo
+    });
+    // change state of video to video clicked
   }
 
   render() {
 
+    // listen for a click on videoList on a certain video
+    // call a function that changes the state
+    // when the state changes in the function call
+    // set the video being sent into videoPlayer
+
     return (
+
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
@@ -18,10 +32,10 @@ class App extends React.Component {
         </nav>
         <div className="row">
           <div className="col-md-7">
-            <VideoPlayer video={this.props.videos[0]}/>
+            <VideoPlayer video={this.state.video}/>
           </div>
           <div className="col-md-5">
-            <VideoList videos={this.props.videos}/>
+            <VideoList videos={this.props.videos} test={this.test}/>
           </div>
         </div>
       </div>
